0f3c70280878765acbaec2e3b5a65eda23dd4edf
Patrick379 <Patrick379@Aol.com>
2017-02-20 13:58:28 -0500

    Colored Pickups
    Put colors on the pickups and added thee points system to the project

--- Assets/Scripts/Controls.cs
+++ Assets/Scripts/Controls.cs
@@ -6,10 +6,12 @@ using UnityEngine;
 public class Controls : MonoBehaviour {
 
     private Rigidbody rb;
+    private int points;
 
     // Use this for initialization
     void Start () {
         rb = GetComponent<Rigidbody>();
+        points = 0;
     }
 	
 	// Update is called once per frame
@@ -27,6 +29,7 @@ public class Controls : MonoBehaviour {
     {
         if (Input.GetKey(KeyCode.Space))
         {
+            
             rb.AddForce(Vector3.up * Time.deltaTime * 1500);
         }
     }
@@ -34,23 +37,30 @@ public class Controls : MonoBehaviour {
     {
         if(other.name == "Goal")
         {
-            GameObject camera = GameObject.Find("Main Camera");
-            Rigidbody rb = GetComponent<Rigidbody>();
-            GameObject cube = GameObject.Find("Cube");
-            
-            transform.position = new Vector3(0, 4, -1);
-            rb.useGravity = false;
-            rb.velocity = Vector3.zero;
-            rb.angularVelocity = Vector3.zero;
-            rb.AddForce(Vector3.up * Time.deltaTime * 5000);
-            
-            camera.GetComponent<CameraController>().enabled = false;
-            camera.transform.position = new Vector3(0, 10, -10);
-            cube.GetComponent<tilt>().enabled = false;
-            cube.transform.position = new Vector3(0, 0, 0);
+            if (points < 1) {
+                GameObject camera = GameObject.Find("Main Camera");
+                Rigidbody rb = GetComponent<Rigidbody>();
+                GameObject cube = GameObject.Find("Cube");
+
+                transform.position = new Vector3(0, 4, -1);
+                rb.useGravity = false;
+                rb.velocity = Vector3.zero;
+                rb.angularVelocity = Vector3.zero;
+                rb.AddForce(Vector3.up * Time.deltaTime * 5000);
+
+                camera.GetComponent<CameraController>().enabled = false;
+                camera.transform.position = new Vector3(0, 10, -10);
+                cube.GetComponent<tilt>().enabled = false;
+                cube.transform.position = new Vector3(0, 0, 0);
 
 
-            Debug.Log("You Win!!");
+                Debug.Log("You Win!!");
+            }
+        }
+        else if(other.name.Contains("Pickup"))
+        {
+            other.gameObject.SetActive(false);
+            points++;
         }
     }
 }
